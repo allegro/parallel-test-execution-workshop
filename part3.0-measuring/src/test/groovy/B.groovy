12bcb06733080e9e00fadd1488137d7e1df97b28
Marcin Mielnicki <marcin.mielnicki@allegro.pl>
2023-06-09 14:49:25 +0200

    Use sleep 1 in measuring

--- part3.0-measuring/src/test/groovy/B.groovy
+++ part3.0-measuring/src/test/groovy/B.groovy
@@ -2,7 +2,7 @@ import spock.lang.Specification
 
 class B extends Specification {
 
-    private static final int SLEEP_DURATION = 200
+    private static final int SLEEP_DURATION = 1
 
     def "test 1"() {
         sleep SLEEP_DURATION
