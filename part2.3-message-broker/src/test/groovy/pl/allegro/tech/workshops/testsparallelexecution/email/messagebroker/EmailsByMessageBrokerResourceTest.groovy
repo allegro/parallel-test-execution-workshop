0f2fd3a3b400da2b51d0fb8fa90ed26a5676a47f
Marcin Mielnicki <marcin.mielnicki@allegro.pl>
2023-06-21 16:53:59 +0200

    Add hint in EmailsByMessageBrokerResourceTest

--- part2.3-message-broker/src/test/groovy/pl/allegro/tech/workshops/testsparallelexecution/email/messagebroker/EmailsByMessageBrokerResourceTest.groovy
+++ part2.3-message-broker/src/test/groovy/pl/allegro/tech/workshops/testsparallelexecution/email/messagebroker/EmailsByMessageBrokerResourceTest.groovy
@@ -1,12 +1,15 @@
 package pl.allegro.tech.workshops.testsparallelexecution.email.messagebroker
 
-import org.junit.Rule
+import org.junit.ClassRule
 import org.springframework.beans.factory.annotation.Value
 import pl.allegro.tech.hermes.mock.HermesMockRule
 import pl.allegro.tech.workshops.testsparallelexecution.BaseTestWithRest
+import spock.lang.Shared
 
 import static java.time.Duration.ofMillis
-import static org.springframework.http.HttpStatus.*
+import static org.springframework.http.HttpStatus.BAD_REQUEST
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR
+import static org.springframework.http.HttpStatus.OK
 import static pl.allegro.tech.hermes.mock.exchange.Response.Builder.aResponse
 
 class EmailsByMessageBrokerResourceTest extends BaseTestWithRest {
@@ -14,28 +17,39 @@ class EmailsByMessageBrokerResourceTest extends BaseTestWithRest {
     @Value('${application.services.message-broker.topic}')
     private String topic
 
-    @Rule
+    @ClassRule
+    @Shared
     private HermesMockRule hermesMock = new HermesMockRule(8089)
 
     private String subject = "New workshops!"
 
+    def cleanup() {
+        hermesMock.resetReceivedRequest()
+    }
+
     def "send e-mail"() {
         given:
         def email = EmailRequest.of(subject, "from@example.com", "to@example.com")
-        hermesMock.define().jsonTopic(topic)
+        /**
+         * Replace with {@link pl.allegro.tech.hermes.mock.HermesMockDefine#jsonTopic(java.lang.String, pl.allegro.tech.hermes.mock.exchange.Response, java.lang.Class, java.util.function.Predicate)}
+         */
+        hermesMock.define().jsonTopic(topic, aResponse().build())
 
         when:
         def result = restClient.post("/emails", email, EmailRequest)
 
         then:
         result.statusCode == OK
+        /**
+         * Replace with {@link pl.allegro.tech.hermes.mock.HermesMockExpect#jsonMessagesOnTopicAs(java.lang.String, int, java.lang.Class, java.util.function.Predicate)}
+         */
         hermesMock.expect().singleJsonMessageOnTopicAs(topic, EmailServiceEvent)
     }
 
     def "do not sent email without sender"() {
         given:
         def email = EmailRequest.of(subject, sender, "to@example.com")
-        hermesMock.define().jsonTopic(topic)
+        hermesMock.define().jsonTopic(topic, aResponse().build())
 
         when:
         def result = restClient.post("/emails", email, EmailRequest)
