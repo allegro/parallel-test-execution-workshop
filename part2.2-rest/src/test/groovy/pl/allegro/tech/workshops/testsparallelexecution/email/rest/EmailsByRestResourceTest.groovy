f91a8fd18a9c50e961eee3e6e74d54abbe13d48a
Marcin Mielnicki <marcin.mielnicki@allegro.pl>
2023-06-21 18:52:07 +0200

    Use Wiremock API in EmailsByRestResourceTest

--- part2.2-rest/src/test/groovy/pl/allegro/tech/workshops/testsparallelexecution/email/rest/EmailsByRestResourceTest.groovy
+++ part2.2-rest/src/test/groovy/pl/allegro/tech/workshops/testsparallelexecution/email/rest/EmailsByRestResourceTest.groovy
@@ -1,18 +1,26 @@
 package pl.allegro.tech.workshops.testsparallelexecution.email.rest
 
 import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder
+import com.github.tomakehurst.wiremock.stubbing.Scenario
 import pl.allegro.tech.workshops.testsparallelexecution.BaseTestWithRest
-import pl.allegro.tech.workshops.testsparallelexecution.support.FaultResponse
-import pl.allegro.tech.workshops.testsparallelexecution.support.Request
-import pl.allegro.tech.workshops.testsparallelexecution.support.Response
 import spock.lang.Shared
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo
+import static com.github.tomakehurst.wiremock.client.WireMock.post
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
 import static com.github.tomakehurst.wiremock.http.Fault.CONNECTION_RESET_BY_PEER
 import static com.github.tomakehurst.wiremock.http.Fault.EMPTY_RESPONSE
-import static java.time.Duration.ofMillis
-import static org.springframework.http.HttpStatus.*
+import static org.springframework.http.HttpHeaders.ACCEPT
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE
+import static org.springframework.http.HttpStatus.BAD_REQUEST
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR
+import static org.springframework.http.HttpStatus.OK
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 
-class EmailsByRestResourceTest extends BaseTestWithRest implements EmailServerStub {
+class EmailsByRestResourceTest extends BaseTestWithRest {
 
     @Shared
     WireMockServer wiremockServer
@@ -36,27 +44,41 @@ class EmailsByRestResourceTest extends BaseTestWithRest implements EmailServerSt
     def "send e-mail"() {
         given:
         def email = EmailRequest.of(subject, "from@example.com", "to@example.com")
-        stubPostJson(new Request(path: "/external-api-service/emails"), Response.OK)
+        wiremockServer.stubFor(post(urlEqualTo("/external-api-service/emails"))
+                .willReturn(aResponse()
+                        .withHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+                        .withStatus(200)
+                )
+        )
 
         when:
         def result = restClient.post("/emails", email, EmailRequest)
 
         then:
         result.statusCode == OK
-        verifyPostJson(new Request(path: "/external-api-service/emails"))
+        wiremockServer.verify(postRequestedFor(urlEqualTo("/external-api-service/emails"))
+                .withHeader(ACCEPT, equalTo("application/json, application/*+json"))
+        )
     }
 
     def "do not sent email without sender"() {
         given:
         def email = EmailRequest.of(subject, sender, "to@example.com")
-        stubPostJson(new Request(path: "/external-api-service/emails"), Response.OK)
+        wiremockServer.stubFor(post(urlEqualTo("/external-api-service/emails"))
+                .willReturn(aResponse()
+                        .withHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+                        .withStatus(200)
+                )
+        )
 
         when:
         def result = restClient.post("/emails", email, EmailRequest)
 
         then:
         result.statusCode == BAD_REQUEST
-        verifyNoPostJson(new Request(path: "/external-api-service/emails"))
+        wiremockServer.verify(0, postRequestedFor(urlEqualTo("/external-api-service/emails"))
+                .withHeader(ACCEPT, equalTo("application/json, application/*+json"))
+        )
 
         where:
         sender << [null, '', ' ']
@@ -64,7 +86,9 @@ class EmailsByRestResourceTest extends BaseTestWithRest implements EmailServerSt
 
     def "handle email service errors"() {
         def email = EmailRequest.of(subject, "from@example.com", "to@example.com")
-        stubPostJson(new Request(path: "/external-api-service/emails"), errorResponse)
+        wiremockServer.stubFor(post(urlEqualTo("/external-api-service/emails"))
+                .willReturn(aResponse().tap(errorResponseBuilder))
+        )
 
         when:
         def result = restClient.post("/emails", email, EmailRequest)
@@ -73,18 +97,34 @@ class EmailsByRestResourceTest extends BaseTestWithRest implements EmailServerSt
         result.statusCode == INTERNAL_SERVER_ERROR
 
         where:
-        errorResponse << [
-                new Response(status: 400),
-                new Response(status: 500),
-                new FaultResponse(fault: EMPTY_RESPONSE),
-                new FaultResponse(fault: CONNECTION_RESET_BY_PEER),
-                new Response(delay: ofMillis(1000)),
+        errorResponseBuilder << [
+                { ResponseDefinitionBuilder builder -> builder.withStatus(400) },
+                { ResponseDefinitionBuilder builder -> builder.withStatus(500) },
+                { ResponseDefinitionBuilder builder -> builder.withFault(EMPTY_RESPONSE) },
+                { ResponseDefinitionBuilder builder -> builder.withFault(CONNECTION_RESET_BY_PEER) },
+                { ResponseDefinitionBuilder builder ->
+                    builder.withFixedDelay(1000)
+                            .withHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+                            .withStatus(200)
+                }
         ]
     }
 
     def "retry email sending"() {
         def email = EmailRequest.of(subject, "from@example.com", "to@example.com")
-        stubPostJson(new Request(path: "/external-api-service/emails"), [errorResponse, Response.OK])
+        wiremockServer.stubFor(post(urlEqualTo("/external-api-service/emails"))
+                .willReturn(aResponse()
+                        .withHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+                        .withStatus(200)
+                ).inScenario('retry scenario')
+                .whenScenarioStateIs(Scenario.STARTED)
+                .willSetStateTo('after error')
+        )
+        wiremockServer.stubFor(post(urlEqualTo("/external-api-service/emails"))
+                .willReturn(aResponse().tap(errorResponseBuilder)).inScenario('retry scenario')
+                .whenScenarioStateIs('after error')
+                .willSetStateTo('after ok')
+        )
 
         when:
         def result = restClient.post("/emails", email, EmailRequest)
@@ -93,12 +133,16 @@ class EmailsByRestResourceTest extends BaseTestWithRest implements EmailServerSt
         result.statusCode == OK
 
         where:
-        errorResponse << [
-                new Response(status: 400),
-                new Response(status: 500),
-                new FaultResponse(fault: EMPTY_RESPONSE),
-                new FaultResponse(fault: CONNECTION_RESET_BY_PEER),
-                new Response(delay: ofMillis(1000)),
+        errorResponseBuilder << [
+                { ResponseDefinitionBuilder builder -> builder.withStatus(400) },
+                { ResponseDefinitionBuilder builder -> builder.withStatus(500) },
+                { ResponseDefinitionBuilder builder -> builder.withFault(EMPTY_RESPONSE) },
+                { ResponseDefinitionBuilder builder -> builder.withFault(CONNECTION_RESET_BY_PEER) },
+                { ResponseDefinitionBuilder builder ->
+                    builder.withFixedDelay(1000)
+                            .withHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+                            .withStatus(200)
+                }
         ]
     }
 }
